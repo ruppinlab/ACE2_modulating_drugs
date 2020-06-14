7f8b88b67b3a314b53afdd751830c8ba9267ad8d
Sanju Sinha <sanju@terpmail.umd.edu>
2020-06-14 14:25:55 -0400

    Replicate figures

---
+++ tools/Create_Figure2.R
@@ -0,0 +1,109 @@
+# This script is used to generate Figure 2-main text and Figure EV3
+require(ggplot2)
+require(ggpubr)
+library(gridExtra)
+library(grid)
+library(ggplot2)
+library(lattice)
+
+setwd('/Users/sinhas8/Project_COVID19/')
+################################################
+# Panel 2A
+################################################
+new_collection=readRDS('collected.results.with.old.ones.RDS')
+new_collection_df=data.frame(padj=new_collection$padj, 
+                             logfc=new_collection$log.fc,
+                             drugName=new_collection$drug)
+# Setting a max cap on the LFC change
+new_collection_df$logfc[new_collection_df$logfc>999]=5
+new_collection_df$point_color=''
+new_collection_df[new_collection_df$logfc > 0 & -log(new_collection_df$padj, 10)>1.3,]$point_color='positive'
+new_collection_df[new_collection_df$logfc < -0 & -log(new_collection_df$padj, 10)>1.3,]$point_color='negative'
+Figure2A<-ggplot(new_collection_df, aes(y= -log(padj, 10), x=logfc, color=point_color))+
+  geom_point(size=4)+
+  theme_bw(base_size = 20)+
+  labs(y='-log10(P)', x='ACE2 expression logFC')+
+  geom_hline(yintercept = 1.3, linetype='dashed', size=1.5, color='blue')+
+  geom_label_repel(data=subset(new_collection_df, -log(padj, 10)>1.3),
+                   aes(label=paste(drugName)), size=6)+
+  ggtitle('Lung in vivo & in vitro from GEO')+
+  theme(legend.position = 'none')+
+  annotate("text", x = -1.5, y = 1.4, label = "P < 0.05", color='blue', size=5)+
+  scale_color_manual(values = c('black', '#F8766D', '#00BFC4'))
+################################################
+# Panel 2B
+################################################
+df_completev2=readRDS('figure2B_Data.RDS')
+tiff('Figure2C_nonCancer.tiff',width = 500, height=700)
+Figure2B<-ggplot(df_completev2[df_completev2$whether_cancer=='non-cancer',],
+                 aes(y= ACE2, fill=Group,x=drug, label=pval))+
+  geom_boxplot()+
+  geom_text()+
+  theme_bw(base_size = 20)+
+  theme(legend.position = 'top',
+        axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())+
+  labs(y='ACE2 Expression')+
+  facet_wrap(~drug_andTissue+GEO, shrink = T, scales = 'free', nrow = 2)
+dev.off()
+################################################
+# Panel 2C
+################################################
+kidney_collection=readRDS('kidney.collected.results.RDS')
+kidney_collection_df=data.frame(padj=kidney_collection$padj, 
+                                logfc=kidney_collection$log.fc,
+                                drugName=kidney_collection$drug)
+kidney_collection_df$drugName=as.character(kidney_collection_df$drugName)
+kidney_collection_df[grep('cisplatin',kidney_collection_df$drug),]
+kidney_collection_df$drugName[grep('cisplatin',kidney_collection_df$drugName)][1:2]=
+  c('cisplatin (Renal\n cortex, rat)', 'cisplatin (Whole\n kidney, mice)')
+kidney_collection_df$drugName=factor(kidney_collection_df$drugName)
+kidney_collection_df$logfc[kidney_collection_df$logfc>9]=5
+kidney_collection_df$point_color=''
+kidney_collection_df[kidney_collection_df$logfc > 0 & -log(kidney_collection_df$padj, 10)>1.3,]$point_color='positive'
+kidney_collection_df[kidney_collection_df$logfc < -0 & -log(kidney_collection_df$padj, 10)>1.3,]$point_color='negative'
+Figure2C<-ggplot(kidney_collection_df, aes(y= -log(padj, 10), x=logfc, color=point_color))+
+  geom_point(size=4)+
+  theme_bw(base_size = 20)+
+  labs(y='-log10(P)', x='ACE2 expression logFC')+
+  geom_hline(yintercept = 1.3, linetype='dashed', size=1.5, color='blue')+
+  geom_label_repel(data=subset(kidney_collection_df, -log(padj, 10)>1.3),
+                   aes(label=paste(drugName)), size=6)+
+  ggtitle('Kidney in vivo & in vitro from GEO')+
+  theme(legend.position = 'none')+
+  annotate("text", x = -0.50, y = 1.4, label = "P < 0.05", color='blue', size=5)+
+  scale_color_manual(values = c('black', '#F8766D', '#00BFC4'))
+################################################
+# Plot
+################################################
+df_complete_kidney=readRDS('figure2B_Data.RDS')
+tiff('Figure2D_Kidney.tiff',width = 600, height=900)
+Figure2D<-ggplot(df_complete_kidney, aes(y= ACE2, fill=Group,x=drug, label=pval))+
+  geom_boxplot()+
+  geom_text()+
+  theme_bw(base_size = 20)+
+  theme(legend.position = 'top',
+        axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())+
+  labs(y='ACE2 Expression')+
+  facet_wrap(~drug_andTissue+GEO, shrink = T, scales = 'free', nrow = 3)
+dev.off()
+#################
+# Figure EV3
+#################
+tiff('EV3_Cancer.tiff',width = 800, height=800)
+ggplot(df_completev2[df_completev2$whether_cancer=='cancer',],
+       aes(y= ACE2, fill=Group,x=drug, label=pval))+
+  geom_boxplot()+
+  geom_text()+
+  theme_bw(base_size = 20)+
+  theme(legend.position = 'top',
+        axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())+
+  labs(y='ACE2 Expression')+
+  facet_wrap(~drug_andTissue+GEO, shrink = T, 
+             scales = 'free', nrow = 3)
+dev.off()
---
+++ tools/create_Figure1.R
@@ -0,0 +1,100 @@
+# Figure 1 without Panel D
+setwd('/Users/sinhas8/Project_COVID19/')
+load('Figure1_new.results.RData')
+require(ggplot2)
+require(ggrepel)
+require(readxl)
+require(fgsea)
+###########################################################################
+# Fig-A
+###########################################################################
+de.res.hyp.carc$point_color=''
+de.res.hyp.carc[de.res.hyp.carc$log.fc > 0 & -log(de.res.hyp.carc$pval, 10)>1,]$point_color='positive'
+de.res.hyp.carc[de.res.hyp.carc$log.fc < -0 & -log(de.res.hyp.carc$pval, 10)>1,]$point_color='negative'
+figA <- ggplot(de.res.hyp.carc, aes(y= -log(pval, 10), x=log.fc, color=point_color))+
+  geom_point(size=4)+
+  theme_bw(base_size = 20)+
+  labs(y='-log10(P)', x='ACE2 expression logFC')+
+  geom_hline(yintercept = 1.3, linetype='dashed', size=1, color='blue')+
+  geom_label_repel(data=subset(de.res.hyp.carc, -log(pval, 10)>1.5),
+                   aes(label=paste(pert_iname)), size=6)+
+  ggtitle('Approved anti-hypertension drugs')+
+  theme(legend.position = 'none')+
+  annotate("text", x = -0.5, y = 1.35, label = "P<0.05", color='blue', size=5)+
+  scale_color_manual(values = c('black', '#F8766D'))
+###########################################################################
+# Fig-B
+###########################################################################
+gsea.res.hyp.carc=get(load('Figure_1B_gsea.res1.RData'))
+gsea.res.hyp.carc$Enriched_in='negative modifier'
+gsea.res.hyp.carc$Enriched_in[gsea.res.hyp.carc$ES>0]='positive modifier'
+gsea.res.hyp.carc=gsea.res.hyp.carc[order(gsea.res.hyp.carc$pval),]
+gsea.res.hyp.carc$Enriched_in=factor(gsea.res.hyp.carc$Enriched_in, labels=c('ACE2 down-regulator',
+                                                                             'ACE2 up-regulator'))
+fig1B<-ggplot(gsea.res.hyp.carc, aes(x=reorder(pathway, -pval, FUN = median),
+                                     y= -log(pval, 10), fill=Enriched_in))+
+  geom_bar(stat = 'identity')+
+  coord_flip()+
+  theme_bw(base_size = 20)+
+  theme(axis.text.y = element_text(angle = 0), legend.position = 'top', legend.title = element_blank(),
+        legend.justification='left', legend.direction='vertical')+
+  labs(x='', y='-log10(P)')+
+  ggtitle('Enrichment by MOA')+
+  geom_hline(yintercept = 1.3, linetype='dashed', size=1.5, color='blue')
+###########################################################################
+# Fig-C
+###########################################################################
+de.res.app.carc$point_color=''
+de.res.app.carc[de.res.app.carc$log.fc > 0 & -log(de.res.app.carc$padj, 10)>1,]$point_color='positive'
+de.res.app.carc[de.res.app.carc$log.fc < -0 & -log(de.res.app.carc$padj, 10)>1,]$point_color='negative'
+figC <- ggplot(de.res.app.carc, aes(y= -log(pval, 10), x=log.fc, color=point_color))+
+  geom_point(size=4)+
+  theme_bw(base_size = 20)+
+  labs(y='-log10(P)', x='ACE2 expression logFC')+
+  geom_hline(yintercept = 1.3, linetype='dashed', size=1.5, color='blue')+
+  geom_label_repel(data=subset(de.res.app.carc, -log(padj, 10)>1),
+                   aes(label=paste(pert_iname)), size=6)+
+  ggtitle('All approved Drugs')+
+  theme(legend.position = 'none')+
+  annotate("text", x = -0.50, y = 1.4, label = "P < 0.05", color='blue', size=5)+
+  scale_color_manual(values = c('black', '#F8766D', '#00BFC4'))
+###########################################################################
+# Fig-E
+###########################################################################
+gsea.res_all_subset=read_xlsx('Figure_1E_topPathways_allClinicalApproveddrugsACE2.xlsx')
+colnames(gsea.res_all_subset)=colnames(gsea.res_all)
+gsea.res_all_subset$Enriched_in='negative modifier'
+gsea.res_all_subset$Enriched_in[gsea.res_all_subset$ES>0]='positive modifier'
+figE<-ggplot(gsea.res_all_subset[1:10,], aes(x=reorder(pathway, -pval), y= -log(pval, 10), fill=Enriched_in))+
+  geom_bar(stat = 'identity')+
+  coord_flip()+
+  theme_bw(base_size = 20)+
+  theme(axis.text.y = element_text(angle = 0), legend.position = 'none', legend.title = element_blank())+
+  labs(x='', y='-log10(P)')+
+  ggtitle('Enrichment by MOA')+
+  geom_hline(yintercept = 1.3, linetype='dashed', size=1.5, color='blue')
+###########################################################################
+# Fig-F
+###########################################################################
+load('gsea.res_Figure1F.RData')
+gsea.res=gsea.res[nchar(pathway)==3 & size>10][, padj:=p.adjust(pval, "BH")][pval<0.05][order(pval)]
+gsea.res=gsea.res[order(gsea.res$pval),]
+gsea.res$Enriched_in='negative modifier'
+gsea.res$Enriched_in[gsea.res$ES>0]='positive modifier'
+gsea.res$name=gsub('AND','&',gsea.res$name)
+figF<-ggplot(gsea.res, aes(x=reorder(tolower(name), -pval), y= -log(pval, 10), fill=Enriched_in))+
+  geom_bar(stat = 'identity')+
+  coord_flip()+
+  theme_bw(base_size = 20)+
+  theme(axis.text.y = element_text(angle = 0),
+        legend.position = 'none', legend.title = element_blank())+
+  labs(x='Indication', y='-log10(P)')+
+  
+  ggtitle('Enrichment by Indication')+
+  geom_hline(yintercept = 1.3, linetype='dashed', size=1.5, color='blue')
+###########################################################################
+# "One func to combine them all" - LOTR
+###########################################################################
+pdf('FIG1_Compv10_wdLabels.pdf', width = 18, height=24)
+plot_grid(figA,  fig1B, figC,figE, figF, nrow=3,  labels = c('A', 'B', 'C', 'D', 'E'), label_size = 32)
+dev.off()
